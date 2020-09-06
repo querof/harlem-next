e282e14eae6a280564aa495de127f824ac9c3dee
Frank Quero <frank.querorobles@coolblue.nl>
2020-09-06 17:42:43 +0200

    [harlen-next] Remove test section (clean recursive)

--- CategoryTree.php
+++ CategoryTree.php
@@ -30,15 +30,3 @@ class CategoryTree
         }), 'category');
     }
 }
-
-
-$c = new CategoryTree();
-$c->addCategory('A', null);
-//$c->addCategory('A', null);
-$c->addCategory('B', 'A');
-$c->addCategory('C', 'A');
-$c->addCategory('X', 'B');
-$c->addCategory('Y', 'B');
-//$c->addCategory('Y', 'X');
-echo implode(',', $c->getChildren('A'));
-echo implode(',', $c->getChildren('B'));
